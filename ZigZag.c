22118dcea4be2bacac73758291fd678364f56192
Lucas Mateus Menezes Silva <luacsm.99@gmail.com>
2018-07-21 13:28:56 -0300

    Placing code comments in English

--- ZigZag.c
+++ ZigZag.c
@@ -1,24 +1,26 @@
 /*
-Autor: Lucas Mateus Menezes Silva
-Função que verifica se uma árvore binária é "Zig_Zag", 1- É "Zig_Zag", 0- Não é "Zig_Zag"
+Author: Lucas Mateus Menezes Silva
+This function checks if a binary search tree is a ZigZag-like tree
+Receives one pointer to the root of the tree
+Return 1- Is "Zig_Zag", 0- Isn't "Zig_Zag"
 */
 
-int Zig_Zag(NoArv *arvore){	//Recebe a raiz da árvore a ser verificada
-	if((arvore->esq != NULL) && (arvore->dir != NULL)){		//No cheio
+int Zig_Zag(NoArv *arvore){	//Receiving the root of the tree
+	if((arvore->esq != NULL) && (arvore->dir != NULL)){		//Full node
 		return 0;
-	}else if((arvore->esq == NULL) && (arvore->dir == NULL)){	//No vazio
+	}else if((arvore->esq == NULL) && (arvore->dir == NULL)){	//Empty node
 		return 0;
 	}else{
-		if(arvore->esq != NULL){	//Se somente o no a esquerda existir
+		if(arvore->esq != NULL){	//If just left node exist
 			Aux = arvore->esq;
-			if((Aux->esq == NULL) && (Aux->dir == NULL)){	//Se o proximo No estiver vazio
+			if((Aux->esq == NULL) && (Aux->dir == NULL)){	//If next node is empty
 				return 1;
 			}else{
 				Zig_Zag(arvore->esq);
 			}
 		}else if(arvore->dir != NULL){
 			Aux = arvore->dir;
-			if((Aux->esq == NULL) && (Aux->dir == NULL)){	//Se o proximo No estiver vazio
+			if((Aux->esq == NULL) && (Aux->dir == NULL)){	//If next node is empty
 				return 1;
 			}else{
 				Zig_Zag(arvore->dir);
